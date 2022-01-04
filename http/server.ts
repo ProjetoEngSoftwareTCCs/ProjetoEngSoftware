8364ea10410c5fab9f74ff06c0739743c167bfe6
PedroCalabria <pedsancal@gmail.com>
2022-01-04 09:39:36 +0000

    tela_orientador

--- http/server.ts
+++ http/server.ts
@@ -4,9 +4,12 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
+
+app.use(express.static(__dirname + '/public/style'))
 
 app.listen(3000)
 console.log('Rodando')
