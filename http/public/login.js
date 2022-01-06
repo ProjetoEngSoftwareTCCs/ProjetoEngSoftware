f71c16199dd47d3983aada976af15953c4c50025
PedroCalabria <pedsancal@gmail.com>
2022-01-06 03:21:04 +0000

    ata_de_banca

--- http/public/login.js
+++ http/public/login.js
@@ -5,46 +5,23 @@
                 <input type="password" id="password" class="input-padrao" required placeholder="suaSenha123#">
                 <input type="submit" value="Entrar" class="enviar">
             </form>*/
-
-function selecionarFormaLogin(usuario){
+function selecionarFormaLogin(usuario) {
     var opcoes = document.getElementById('opcoes');
-    var botao = document.getElementById('botao_alterar_login')
-    if (usuario === "aluno"){
-        opcoes.innerHTML = `<form class="login" action="aluno.html">
-            <label for="email">Email: </label>
-            <input type="email" id="email" class="input-padrao" required placeholder="seuemail@dominio.com">
-            <label for="password">Senha: </label>
-            <input type="password" id="password" class="input-padrao" required placeholder="suaSenha123#">
-            <input type="submit" value="Entrar" class="enviar">
-            </form>`
-    } else if(usuario === "orientador"){
-        opcoes.innerHTML = `<form class="login" action="orientador.html">
-        <label for="email">Email: </label>
-        <input type="email" id="email" class="input-padrao" required placeholder="seuemail@dominio.com">
-        <label for="password">Senha: </label>
-        <input type="password" id="password" class="input-padrao" required placeholder="suaSenha123#">
-        <input type="submit" value="Entrar" class="enviar">
-        </form>`
-    } else if(usuario === "administrador"){
-        opcoes.innerHTML = `<form class="login" action="administrador.html">
-        <label for="email">Email: </label>
-        <input type="email" id="email" class="input-padrao" required placeholder="seuemail@dominio.com">
-        <label for="password">Senha: </label>
-        <input type="password" id="password" class="input-padrao" required placeholder="suaSenha123#">
-        <input type="submit" value="Entrar" class="enviar">
-        </form>`
+    var botao = document.getElementById('botao_alterar_login');
+    if (usuario === "aluno") {
+        opcoes.innerHTML = "<form class=\"login\" action=\"aluno.html\">\n            <label for=\"email\">Email: </label>\n            <input type=\"email\" id=\"email\" class=\"input-padrao\" required placeholder=\"seuemail@dominio.com\">\n            <label for=\"password\">Senha: </label>\n            <input type=\"password\" id=\"password\" class=\"input-padrao\" required placeholder=\"suaSenha123#\">\n            <input type=\"submit\" value=\"Entrar\" class=\"enviar\">\n            </form>";
     }
-
-    botao.innerHTML = " <button class='botao' onclick = 'alterarFormaLogin()' >Alterar Forma de Login</button>"
+    else if (usuario === "orientador") {
+        opcoes.innerHTML = "<form class=\"login\" action=\"orientador.html\">\n        <label for=\"email\">Email: </label>\n        <input type=\"email\" id=\"email\" class=\"input-padrao\" required placeholder=\"seuemail@dominio.com\">\n        <label for=\"password\">Senha: </label>\n        <input type=\"password\" id=\"password\" class=\"input-padrao\" required placeholder=\"suaSenha123#\">\n        <input type=\"submit\" value=\"Entrar\" class=\"enviar\">\n        </form>";
+    }
+    else if (usuario === "administrador") {
+        opcoes.innerHTML = "<form class=\"login\" action=\"administrador.html\">\n        <label for=\"email\">Email: </label>\n        <input type=\"email\" id=\"email\" class=\"input-padrao\" required placeholder=\"seuemail@dominio.com\">\n        <label for=\"password\">Senha: </label>\n        <input type=\"password\" id=\"password\" class=\"input-padrao\" required placeholder=\"suaSenha123#\">\n        <input type=\"submit\" value=\"Entrar\" class=\"enviar\">\n        </form>";
+    }
+    botao.innerHTML = " <button class='botao' onclick = 'alterarFormaLogin()' >Alterar Forma de Login</button>";
 }
-
-function alterarFormaLogin(){
+function alterarFormaLogin() {
     var opcoes = document.getElementById('opcoes');
-    var botao = document.getElementById('botao_alterar_login')
-
-    opcoes.innerHTML = `<button class="botao_login" onclick="selecionarFormaLogin('aluno')">Entrar como Aluno</button>
-    <button class="botao_login" onclick="selecionarFormaLogin('orientador')">Entrar como Orientador</button>
-    <button class="botao_login" onclick="selecionarFormaLogin('administrador')">Entrar como Administrador</button>`
-
-    botao.innerHTML = ""
+    var botao = document.getElementById('botao_alterar_login');
+    opcoes.innerHTML = "<button class=\"botao_login\" onclick=\"selecionarFormaLogin('aluno')\">Entrar como Aluno</button>\n    <button class=\"botao_login\" onclick=\"selecionarFormaLogin('orientador')\">Entrar como Orientador</button>\n    <button class=\"botao_login\" onclick=\"selecionarFormaLogin('administrador')\">Entrar como Administrador</button>";
+    botao.innerHTML = "";
 }
